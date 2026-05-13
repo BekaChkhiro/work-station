4530847688dc930e7dd47bc797a0b8577a4ef02c
BekaChkhiro <bekachkhirodze1@gmail.com>
2026-05-13 15:45:15 +0400

    style(t18): single-line prettier format for short JSX tab routes

--- mobile/src/routes/tasks.tsx
+++ mobile/src/routes/tasks.tsx
@@ -1,10 +1,5 @@
 import { TabScreen } from "../components/TabScreen";
 
 export default function TasksRoute() {
-  return (
-    <TabScreen
-      title="Tasks"
-      hint="PlanFlow task list lands in T18.15."
-    />
-  );
+  return <TabScreen title="Tasks" hint="PlanFlow task list lands in T18.15." />;
 }
